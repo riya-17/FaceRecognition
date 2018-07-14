3758290a4cb202dd1f423f08bcc1e093a18d93fd
Riya <riyad17031998@gmail.com>
2018-07-14 15:04:46 +0530

    Add File FaceLandmark.py

---
+++ FaceLandmark.py
@@ -0,0 +1,13 @@
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
