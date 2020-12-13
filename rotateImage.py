e88d27a9fef09c6501c8cb3f3474c99cd516e00b
js04181 <park04181@gmail.com>
2020-12-14 02:34:13 +0900

    enhance rotate function(remove black box)

--- rotateImage.py
+++ rotateImage.py
@@ -1,41 +1,51 @@
 import cv2
-
-# read image as grey scale
-
-def rotateFunction(img):
-
-    # img = cv2.imread(imagePath)
-    # get image height, width
-    (h, w) = img.shape[:2
-             ]
-    # calculate the center of the image
-    center = (w / 2, h / 2)
-
-    angle90 = 90
-    angle180 = 180
-
-    scale = 1.0
-
-    # Perform the counter clockwise rotation holding at the center
-    # 90 degrees
-    M = cv2.getRotationMatrix2D(center, angle90, scale)
-    rotated90 = cv2.warpAffine(img, M, (h, w))
-
-    # 180 degrees
-    M = cv2.getRotationMatrix2D(center, angle180, scale)
-    rotated180 = cv2.warpAffine(img, M, (w, h))
-
-    cv2.imshow('Image rotated by 90 degrees', rotated90)
-    cv2.waitKey(0)  # waits until a key is pressed
-    cv2.destroyAllWindows()  # destroys the window showing image
-
-    cv2.imshow('Image rotated by 180 degrees', rotated180)
-    cv2.waitKey(0)  # waits until a key is pressed
-    cv2.destroyAllWindows()  # destroys the window showing image
-
-    return rotated90, rotated180
+import numpy as np
 
 
-if __name__ == '__main__':
-    image = "C:\\Users\\KSHITIJ\\PycharmProjects\\rotateImage\\virat.jpg"
-    rotateFunction(image)
+# rotation of the image
+def rotate_image(image, angle):
+    # Size of the image input
+    size = (image.shape[1], image.shape[0])
+    center_of_image = tuple(np.array(size) / 2)
+    rotation = np.vstack(
+        [cv2.getRotationMatrix2D(center_of_image, angle, 1.0), [0, 0, 1]]
+    )
+
+    rotation2 = np.matrix(rotation[0:2, 0:2])
+    width = size[0] * 0.5
+    height = size[1] * 0.5
+    rotated_coordinates = [
+        (np.array([-width, height]) * rotation2).A[0],
+        (np.array([width, height]) * rotation2).A[0],
+        (np.array([-width, -height]) * rotation2).A[0],
+        (np.array([width, -height]) * rotation2).A[0]
+    ]
+
+    # Size of the image output
+    x_coordinates = [pt[0] for pt in rotated_coordinates]
+    x_positif = [x for x in x_coordinates if x > 0]
+    x_negatif = [x for x in x_coordinates if x < 0]
+    y_coordinates = [pt[1] for pt in rotated_coordinates]
+    y_positif = [y for y in y_coordinates if y > 0]
+    y_negatif = [y for y in y_coordinates if y < 0]
+    right = max(x_positif)
+    left = min(x_negatif)
+    top = max(y_positif)
+    bottom = min(y_negatif)
+    new_width = int(abs(right - left))
+    new_height = int(abs(top - bottom))
+    translation_matrix = np.matrix([
+        [1, 0, int(new_width * 0.5 - width)],
+        [0, 1, int(new_height * 0.5 - height)],
+        [0, 0, 1]
+    ])
+
+    compute_matrix = (np.matrix(translation_matrix) * np.matrix(rotation))[0:2, :]
+    result = cv2.warpAffine(
+        image,
+        compute_matrix,
+        (new_width, new_height),
+        flags=cv2.INTER_LINEAR
+    )
+
+    return result
